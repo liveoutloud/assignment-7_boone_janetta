5fecdc6f6706d182951f7fb25c09c13d0520e873
Janetta Boone <j.boone@ufl.edu>
2018-07-16 06:49:07 -0500

    h2 headings var

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@ $("img").remove();
 $(document).ready(function(){
 
 });
+
+$(document).ready(function(){
+    var headings = $("h2");
+    headings.last().text("I\'m Janetta Boone, and I\'m learning jQuery!")
+});
