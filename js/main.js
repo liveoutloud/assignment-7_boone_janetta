601da83430439740657563ba2262644e382d766e
Janetta Boone <j.boone@ufl.edu>
2018-07-16 06:51:55 -0500

    styles to p

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ $(document).ready(function(){
     var headings = $("h2");
     headings.last().text("I\'m Janetta Boone, and I\'m learning jQuery!")
 });
+
+$(document).ready(function(){
+    var paragraphs = $("p");
+    paragraphs.css({
+        'font-size': '18px',
+        'color': '#0B333F',
+        'background-color': '#DAF7A6',
+        'padding': '10px',
+        'border-radius': '8px'
+    });
+});
